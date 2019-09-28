2ebd3562e4adee973eb33def6647c11340ed908a
js13kGames <contact@js13kgames.com>
2019-09-28 02:27:00 +0100

    :joy_cat: Identity ddb schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,15 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `identity`;
+CREATE TABLE `identity` (
+  `user_id` int(11) NOT NULL,
+  `identity_id` text NOT NULL,
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `identity_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
