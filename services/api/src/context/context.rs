73e9d00e898978f5023935583bfcef0f4b0dd54a
js13kGames <contact@js13kgames.com>
2019-09-28 03:21:00 +0100

    :hearts: FromRequest trait for Context

--- services/api/src/context/context.rs
+++ services/api/src/context/context.rs
@@ -6,3 +6,16 @@ use rocket::{Request};
 pub struct Context { }
 
 impl juniper::Context for Context {}
+
+#[derive(Debug)]
+pub enum ContextError {
+    Example
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for Context {
+    type Error = ContextError;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        Outcome::Success(Context { })
+    }
+}
