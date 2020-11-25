ada9a14c35a29b9731895193ccb89781ac7f315b
js13kGames <contact@js13kgames.com>
2020-11-25 22:58:30 +0100

    install typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+                 <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head>   
 
       <main className={styles.main}>
         <h1 className={styles.title}>
