193ede9c886725b9c9e6346a18df314554834c26
js13kGames <contact@js13kgames.com>
2020-11-29 21:43:12 +0100

    clean code

--- pages/api/hello/index.ts
+++
@@ -1,8 +0,0 @@
-import { map_routes, RouteHandlerMethods } from '../../../server';
-
-export default map_routes({
-	[RouteHandlerMethods.POST]: (req, res) => {
-		res.statusCode = 200;
-		res.json({ name: 'John Doe' });
-	}
-});
