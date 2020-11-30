1f2eba86387ef683665addce97aa684c9259f9b0
js13kGames <contact@js13kgames.com>
2020-11-30 02:17:13 +0100

    seed default menu items

--- migrator/migrations/V003__Add_menu_items_table.sql
+++ migrator/migrations/V003__Add_menu_items_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE PUBLIC.menu_items
     id UUID DEFAULT Gen_random_uuid() PRIMARY KEY,
     title CITEXT NOT NULL,
     url CITEXT NOT NULL,
+    active boolean DEFAULT false NOT NULL,
     created_at TIMESTAMP WITH time zone DEFAULT Now() NOT NULL,
     updated_at TIMESTAMP WITH time zone DEFAULT Now() NOT NULL
 );
---
+++ migrator/migrations/V004__Add_default_menu_items.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    "public"."menu_items" ("title", "url", "active")
+VALUES
+    ('Winners', '/winners', 'true'),
+    ('Entries', '/entries', 'true'),
+    ('Partners', '/partners', 'true'),
+    ('Experts', '/experts', 'true'),
+    ('Prizes', '/prizes', 'true'),
+    ('Rules', '/rules', 'true'),
+    ('Blog', '/blog', 'true'),
+    ('Previous Editions', '/previous_editions', 'true');
