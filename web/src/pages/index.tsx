70e2ccaccac880fe3176c8f9ca550f2fa56ec572
js13kGames <contact@js13kgames.com>
2020-11-30 18:07:07 +0100

    install fontawsome and create hero's template

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,19 +1,64 @@
+import {
+  faFacebookF,
+  faInstagram,
+  faSlack,
+  faTwitter
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from 'next/head';
-import Button from '../components/Button/button';
+import Link from "next/link";
+import Button from "../components/Button/button";
 import styles from '../styles/Home.module.scss';
 
-export const getStaticProps = async () => {
-  const buffer = await fetch('https://dd5070fc-6d5b-4660-90bc-f47c62d45223.mock.pstmn.io/hero_data')
-  const data = await buffer.json();
 
-  return {
-    props: {
-      heroData: data,
-    }
-  }
-}
+const heroData = 
+  {
+    primaryText: "lead sentence here",
+    secondaryText: "compo ends in:",
+    countdownDate: "xx days xx:xx:xx",
+    callToAction: {
+      text: "sumbit the game",
+      url: "http://google.com",
+    },
+    socialLinks: 
+     [ 
+       {
+        title: "twitter",
+        iconName: faTwitter,
+        url: "https://twitter.com/js13kGames"
+      },
+       {
+        title: "facebook",
+        iconName: faFacebookF,
+        url: "https://www.facebook.com/js13kGames/"
+      },
+       {
+        title: "slack",
+        iconName: faSlack,
+        url: "https://js13kgames.slack.com/"
+      },
+       {
+        title: "instagram",
+        iconName: faInstagram,
+        url: "https://www.instagram.com/js13kgames/"
+      },
+     
+    ]
+  };
 
-export default function Home({ heroData }) {
+
+// export const getStaticProps = async () => {
+//   const buffer = await fetch("/homeTestApi")
+//   const data = await buffer.json();
+
+//   return {
+//     props: {
+//       heroData: data,
+//     }
+//   }
+// }
+
+export default function Home() {
   return (
 <>
       <Head>
@@ -21,7 +66,17 @@ export default function Home({ heroData }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>   
      <div className={styles.heroContainer}>
-       <p className={styles.socialIcons}>{heroData.socialLinks[1].title}</p>
+       <div className={styles.socialIcons}>
+         {heroData.socialLinks.map(({url, title, iconName}) => 
+(
+  <li className={styles.iconWrapper} key={title}>
+<Link href={url} >
+  <a title={title}><FontAwesomeIcon className={styles.icon} icon={iconName}  /> </a>
+</Link>
+</li>
+)
+         )}
+       </div>
        <div className={styles.middleContainer}>
          <p>{heroData.primaryText}</p>
          <p>{heroData.secondaryText}: {heroData.countdownDate}</p>
