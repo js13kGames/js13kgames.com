92837fea6c63bc8a5c5b8d65bff9ed9c51046896
js13kGames <contact@js13kgames.com>
2020-11-30 03:28:42 +0100

    filter fields in query, not in logic layer

--- web/server/controllers/menuItems/index.ts
+++ web/server/controllers/menuItems/index.ts
@@ -3,14 +3,11 @@ import * as Types from '../../types';
 
 export const getMenuItems: RouteHandler = async (req, res, options) => {
 	const menu_items = <Types.menu_items[]>(
-		await options.db.public.menu_items.find({ active: true })
+		await options.db.public.menu_items.find(
+			{ active: true },
+			{ fields: ['title', 'url', 'id'] }
+		)
 	);
 
-	res.json(
-		menu_items.map(({ title, url, id }) => ({
-			title,
-			url,
-			id
-		}))
-	);
+	res.json(menu_items);
 };
