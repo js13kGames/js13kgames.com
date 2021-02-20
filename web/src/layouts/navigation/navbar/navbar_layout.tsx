f528436ddd5d1739cbafdd062a4d1a2f30bcc25c
js13kGames <contact@js13kgames.com>
2021-02-20 01:54:07 +0100

    logout action + user information

--- web/src/layouts/navigation/navbar/navbar_layout.tsx
+++ web/src/layouts/navigation/navbar/navbar_layout.tsx
@@ -5,12 +5,19 @@ import styles from './navbar.module.scss';
 import NavItem from './navItem';
 
 const NavbarLayout = ({ loading, menuItems, year }) => {
-	if (loading) {
+	const {
+		loginWithRedirect,
+		logout,
+		user,
+		isAuthenticated,
+		isLoading
+	} = useAuth0();
+
+	if (loading || isLoading) {
 		return <h1>Loading</h1>;
 	}
 
-	const { loginWithRedirect } = useAuth0();
-
+	console.log({ user });
 	return (
 		<nav className={styles.navWrapper}>
 			<div className={styles.navWrapperTop}>
@@ -18,7 +25,22 @@ const NavbarLayout = ({ loading, menuItems, year }) => {
 					<a>js13kgames logo</a>
 				</Link>
 				<div className={styles.loginWrapper}>
-					<Button onClick={() => loginWithRedirect()}>login</Button>
+					{isAuthenticated ? (
+						<div className={styles.loggedInWrapper}>
+							<img src={user.picture} className={styles.avatar}></img>
+							<p className={styles.name}>
+								{user.name}
+								<span
+									className={styles.logout}
+									onClick={() => logout({ returnTo: window.location.origin })}
+								>
+									(log out)
+								</span>
+							</p>
+						</div>
+					) : (
+						<Button onClick={() => loginWithRedirect()}>login</Button>
+					)}
 				</div>
 			</div>
 			<div className={styles.navWrapperBottom}>
