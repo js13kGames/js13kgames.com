eda2eb340303207251d1c72226f958090b7c3c31
js13kGames <contact@js13kgames.com>
2021-02-20 01:28:18 +0100

    add auth0 provider to appcontextProvider

--- web/src/core/AppContextProvider.tsx
+++ web/src/core/AppContextProvider.tsx
@@ -1,6 +1,20 @@
+import { Auth0Provider } from '@auth0/auth0-react';
+import getConfig from 'next/config';
 import { FC } from 'react';
 import { ApiProvider } from './ApiProvider';
 
 export const AppContextProvider: FC = ({ children }) => {
-	return <ApiProvider>{children}</ApiProvider>;
+	const {
+		publicRuntimeConfig: { AUTH0_DOMAIN, AUTH0_CLIENT_ID }
+	} = getConfig();
+
+	return (
+		<Auth0Provider
+			domain={AUTH0_DOMAIN}
+			clientId={AUTH0_CLIENT_ID}
+			redirectUri={window.location.origin}
+		>
+			<ApiProvider>{children}</ApiProvider>
+		</Auth0Provider>
+	);
 };
