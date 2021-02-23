89b41c9ced6d271e0d58b9aa95779400c46fd051
js13kGames <contact@js13kgames.com>
2021-02-23 23:45:02 +0100

    import svg and scroll navbar

--- web/next.config.js
+++ web/next.config.js
@@ -1,22 +1,30 @@
 // XXX: This should be grabbed from the API
-const CURRENT_EDITION = '2020';
+const CURRENT_EDITION = "2020";
 
 module.exports = {
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination: '/' + CURRENT_EDITION,
-				permanent: false
-			}
-		];
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/graphql',
-				destination: 'http://graphql:5433/graphql'
-			}
-		];
-	}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/" + CURRENT_EDITION,
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/graphql",
+        destination: "http://graphql:5433/graphql",
+      },
+    ];
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
+  },
 };
--- web/src/layouts/navigation/navbar/navbar_layout.tsx
+++ web/src/layouts/navigation/navbar/navbar_layout.tsx
@@ -1,41 +1,61 @@
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import {useEffect, useState} from "react";
+import Logo from "../../../assets/logo/mainLogo.svg";
+import {Button} from "../../../components";
 import styles from "./navbar.module.scss";
 import NavItem from "./navItem";
 
 const NavbarLayout = ({loading, menuItems, year}) => {
+  const [navScroll, setNavScroll] = useState(false);
+
   if (loading) {
     return <h1>Loading</h1>;
   }
+
+  const listenScrollEvent = () => {
+    if (window.scrollY >= document.getElementById("navbar").offsetHeight) {
+      setNavScroll(true);
+    } else {
+      setNavScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+  }, []);
 
   return (
     <>
       <header className={styles.navHeader}>
         {/* <div className={styles.navWrapperTop}> */}
         <Link href={"/" + year}>
-          <a>js13kgames logo</a>
+          {/* <a>js13kgames logo</a> */}
+          <img className={styles.logo} src={Logo} alt="logo" />
+          {/* <Logo style={{width: "400px"}} /> */}
         </Link>
         <div className={styles.loginWrapper}>
-          <Link href="/register">
-            <a>register</a>
-          </Link>
-          <p> / </p>
           <Link href="/login">
-            <a>login</a>
+            <a className={styles.login}>register | login</a>
           </Link>
+          <Button buttonClass="newSubmit" href="/submit">
+            submit the game
+          </Button>
         </div>
       </header>
       {/* <nav className={styles.navWrapper}> */}
 
-      <nav className={styles.navbar}>
+      <nav
+        id="navbar"
+        className={`${styles.navbar} ${styles[navScroll ? "fixed" : ""]}`}>
         {/* <div > */}
         <ul className={styles.navItems}>
           {menuItems.map(({url, title}) => (
             <NavItem href={`/${year}${url}`} content={title} key={title} />
           ))}
         </ul>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+        <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} />
         {/* </div> */}
       </nav>
     </>
