49f029275521e042beefbb518412aca8bbcdb628
js13kGames <contact@js13kgames.com>
2021-05-27 23:51:37 +0200

    submit template

--- web/src/pages/submit.tsx
+++ web/src/pages/submit.tsx
@@ -1,5 +1,23 @@
+import styles from "../styles/submit.module.scss";
+
 const Submit = () => {
-  return <h1>submit your game</h1>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>submit your game</h1>
+      <main className={styles.middleWrapper}>
+        <div className={styles.left}>
+          <div className={styles.upload}>
+            <p>upload game file</p>
+          </div>
+        </div>
+        <div className={styles.information}></div>
+      </main>
+      <div>
+        <button style={{padding: "20px"}}>elo</button>
+        <button style={{padding: "20px"}}>elo</button>
+      </div>
+    </div>
+  );
 };
 
 export default Submit;
