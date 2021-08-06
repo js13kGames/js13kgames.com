4758fdea6cd17db63428948b7bf589d762226a33
js13kGames <contact@js13kgames.com>
2021-08-06 21:19:51 +0100

    jwt validation logic

---
+++ web/src/utils/validateToken.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
+
+const jwks_uri = `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`;
+
+const client = jwksClient({ jwksUri: jwks_uri });
+
+const get_key = (header: any, callback: Function) => {
+	client.getSigningKey(header.kid, (err: any, key: any) => {
+		if (err) {
+			console.error(`getSigningKey error: ${err}`);
+			return callback(err, null);
+		}
+
+		console.info(`Got signing key for JWT.`);
+		const signing_key = key ? key.publicKey || key.rsaPublicKey : '';
+		callback(null, signing_key);
+	});
+};
+
+export const validate_token = (
+	token: string = ''
+): Promise<Record<string, string>> => {
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, get_key.bind(this), {}, (error: any, decoded: any) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(decoded);
+			}
+		});
+	});
+};
