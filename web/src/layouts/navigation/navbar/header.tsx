bb733b8ac603195ce3a2e6e7fef2bd54f442c630
js13kGames <contact@js13kgames.com>
2021-08-16 21:06:03 +0200

    no undefined year when returning to homepage

--- web/src/layouts/navigation/navbar/header.tsx
+++ web/src/layouts/navigation/navbar/header.tsx
@@ -1,29 +1,29 @@
-import {useAuth0} from "@auth0/auth0-react";
-import Link from "next/link";
-import Logo from "../../../assets/logo/js13kgames-logo.svg";
-import {NavSkeleton} from "../../../components";
-import styles from "./header.module.scss";
-import Login from "./login";
-import Select from "./select";
+import { useAuth0 } from '@auth0/auth0-react';
+import Link from 'next/link';
+import Logo from '../../../assets/logo/js13kgames-logo.svg';
+import { NavSkeleton } from '../../../components';
+import styles from './header.module.scss';
+import Login from './login';
+import Select from './select';
 
-const Header = ({year, years}) => {
-  const {isLoading} = useAuth0();
+const Header = ({ year, years }) => {
+	const { isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <NavSkeleton header />;
-  }
+	if (isLoading) {
+		return <NavSkeleton header />;
+	}
 
-  return (
-    <header id="header" className={styles.navHeader}>
-      <Link href={"/" + year}>
-        <img className={styles.logo} src={Logo} alt="logo" />
-      </Link>
-      <Select yearOptions={years} />
-      <div className={styles.loginWrapper}>
-        <Login />
-      </div>
-    </header>
-  );
+	return (
+		<header id='header' className={styles.navHeader}>
+			<Link href={'/'}>
+				<img className={styles.logo} src={Logo} alt='logo' />
+			</Link>
+			<Select yearOptions={years} />
+			<div className={styles.loginWrapper}>
+				<Login />
+			</div>
+		</header>
+	);
 };
 
 export default Header;
